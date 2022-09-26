13bcc3c2b44f1794145b2411e9f51dc1ffa4f848
carmenuv <carmenurizarv@gmail.com>
2022-09-25 21:12:44 -0500

    Sección Adopción

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Servicios } from "../components/Servicios/index";
-import Adoption from "../components/Adopta";
+import AdoptionSection from "../components/Adoption";
 
 
 
@@ -12,7 +12,7 @@ const Router =()=>{
     
       <Routes>
         <Route path="/" element={<Servicios/>}/>
-        <Route path="/adopcion" element={<Adoption/>}/>
+        <Route path="/adopcionseccion" element={<AdoptionSection/>}/>
       </Routes>
 
 
