a7c611741a1ee9017b3b322fde4364384945201a
Erick1205 <coronado.erick96@gmail.com>
2022-09-25 19:32:27 -0500

    carmen2.0

--- src/components/Adopta/index.js
+++ src/components/Adopta/index.js
@@ -2,13 +2,25 @@ import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 import { apiDataAdoption } from "../../Service/Api";
+import {
+  Container,
+  Grid,
+  FormControl,
+  InputLabel,
+  TextField,
+  Select,
+  MenuItem,
+  Card,
+  CardMedia,
+  CardContent, CircularProgress,
+} from "@mui/material";
+import { Link } from "react-router-dom";
 
+const Adoption = () => {
 
-const Adoption =()=>{
-  
   const [animal, setAnimal] = useState([])
 
-  const fetchAdoptionList = async()=>{
+  const fetchAdoptionList = async () => {
     const listAnimals = await apiDataAdoption();
 
     console.log("listAnimals", listAnimals.pets);
@@ -16,31 +28,68 @@ const Adoption =()=>{
 
   };
 
-  useEffect(()=>{
+  useEffect(() => {
 
     fetchAdoptionList();
-  },[])
+  }, [])
 
 
-  return(
+  return (
 
-    <div>
+    <Container>
+      <Grid container spacing={3} mt={5}>
+        <Grid item md={6}>
+          <TextField
 
-      {animal.map((pet)=>(
-        <div>
-          
-            <img src={pet.image} alt="" />
-            <h1>nombre: {pet.name}</h1>
-            <h2>tipo: {pet.type}</h2>
-            <h4>edad: {pet.age}</h4>
-          
-        </div>
-      ))}
+            label="Busca tu mascota..."
+            fullWidth
+          />
+        </Grid>
+        <Grid item md={6}>
+          <FormControl fullWidth>
+            <InputLabel>Filter by Animal</InputLabel>
+            <Select
+              label="Filtra por Region"
+              value={animal} >
 
-    </div>
+              <MenuItem value="all">raza</MenuItem>
+              <MenuItem value="Africa">edad</MenuItem>
+              <MenuItem value="Americas">tamaño</MenuItem>
 
-  )
+            </Select>
+          </FormControl>
+        </Grid>
 
-}
+        {animal.length > 0 ? (
+          animal.map((pet) => (
+            <Grid item md={3} xs={12}>
+              <Link to={`/adopcion/${pet.name}`}>
+                <Card  ClassName="card">
+                  <CardMedia
+                    component="img"
+                    height={200}
+                    image={pet.image}
+                  />
+                  <CardContent  >
+                    <h1>nombre: {pet.name}</h1>
+                    <h2>tipo: {pet.type}</h2>
+                    <h4>edad: {pet.age}</h4>
+                  </CardContent>
+                </Card>
+              </Link>
+            </Grid>
+
+          ))
+
+        ) : (
+          <div className="center loading">
+            <CircularProgress />
+            <h4>Cargando...</h4>
+          </div>
+        )}
+      </Grid>
+    </Container>
+  );
+};
 
 export default Adoption;
