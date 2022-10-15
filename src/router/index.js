eb5ba88d799fc25c8bdf3e73d5c71d60d0d754af
carmenuv <carmenurizarv@gmail.com>
2022-10-15 08:17:25 -0500

    Prueba 2 routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "../components/Header/Header";
+import Header from "../components/header/Header";
 // import Footer from "../components/Footer/Footer";
 // import Hero from "../components/Hero/Hero";
 // import GetPets from "../components/AdoptaPets/AdoptaPets";
