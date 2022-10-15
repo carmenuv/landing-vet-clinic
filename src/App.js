b1d81910390687e02bfb5809528d490d7603fe96
carmenuv <carmenurizarv@gmail.com>
2022-10-15 12:15:02 -0500

    Cambio App Routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 
-import Router from '../src/router';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header.js";
+import Footer from "./components/Footer/Footer.js";
+import Hero from "./components/Hero/Hero.js";
+import GetPets from "./components/AdoptaPets/AdoptaPets.js";
 
+const App =()=>{
 
-function App() {
-  return (
-    
-      <div>
-        <Router/>
-      </div>      
+  return(
 
-  );
+    <BrowserRouter>
+
+      <Header/>
+      <Routes>        
+        <Route path="/" element={<Hero/>}/>
+        <Route path="/adopta" element={<GetPets/>}/>
+      </Routes>
+      <Footer/>
+      
+    </BrowserRouter>
+
+  )
+
 }
 
 export default App;
--- src/components/Servicios/main.js
+++ src/components/Servicios/main.js
@@ -6,6 +6,6 @@ import ReactDOM from "react-dom";
 import "swiper/css/bundle";
 import "./Servicios.css";
 
-import App from "./App.jsx.js";
+import App from "./App.jsx.js.js";
 
 ReactDOM.render(<App />, document.getElementById("app"));
