c50f993f68440a855e12773e0a0fd4dce3c73c20
carmenuv <carmenurizarv@gmail.com>
2022-10-14 23:49:58 -0500

    Cambio de nombre carpeta components

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "../Components/Header/Header";
-import Footer from "../Components/Footer/Footer";
-import Hero from "../Components/Hero/Hero";
-import GetPets from "../Components/AdoptaPets/AdoptaPets";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+import Hero from "../components/Hero/Hero";
+import GetPets from "../components/AdoptaPets/AdoptaPets";
 
 const Router =()=>{
 
