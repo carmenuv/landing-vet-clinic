64a96094f7f506ad931ae2de8a676e6234da47ab
carmenuv <carmenurizarv@gmail.com>
2022-10-15 08:16:10 -0500

    Prueba Routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Header from "../components/Header/Header";
-import Footer from "../components/Footer/Footer";
-import Hero from "../components/Hero/Hero";
-import GetPets from "../components/AdoptaPets/AdoptaPets";
+import Header from "../components/Header/Header";
+// import Footer from "../components/Footer/Footer";
+// import Hero from "../components/Hero/Hero";
+// import GetPets from "../components/AdoptaPets/AdoptaPets";
 
 const Router =()=>{
 
   return(
 
-    <BrowserRouter>
+    <Header/>
 
-      {/* <Header/> */}
-      <Routes>        
-        <Route path="/" element={<Hero/>}/>
-        <Route path="/adopta" element={<GetPets/>}/>
-      </Routes>
-      <Footer/>
+    // <BrowserRouter>
+
+    //   <Routes>        
+    //     <Route path="/" element={<Hero/>}/>
+    //     <Route path="/adopta" element={<GetPets/>}/>
+    //   </Routes>
+    //   <Footer/>
       
-    </BrowserRouter>
+    // </BrowserRouter>
 
   )
 
